683aa541c1000e2b649416b327a20b41d9cb65c3
ClayBlade <claytonz@nycstudents.net>
2025-07-14 08:20:45 -0400

    today frfr

--- MRIPIDM/helperFuncs.h
+++ MRIPIDM/helperFuncs.h
@@ -1,9 +1,10 @@
 #ifndef helperFuncs_H
 #define helperFuncs_H
 
+extern 'c' {
  float* ippsMalloc_32fHF(int len) ;
  void ippsFreeHF(float* ptr);
  void ippsZero_64fHF(double* ptr, int len);
  void ippsSum_32fHF(const float* src, int len, float* sum);
-
+}
 #endif
