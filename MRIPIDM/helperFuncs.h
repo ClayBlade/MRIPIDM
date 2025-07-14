e7fc74395e932f602ec382e2c8a7a96e5bef19d9
ClayBlade <claytonz@nycstudents.net>
2025-07-13 20:47:25 -0400

    Changed the turtle water

--- MRIPIDM/helperFuncs.h
+++ MRIPIDM/helperFuncs.h
@@ -1,15 +1,16 @@
-
-
-#pragma once
-
 #ifdef __CUDACC__  // If compiling with nvcc (CUDA)
     typedef int IppAlgHint;
     #define ippAlgHintFast 0
 #else
     #include <ipps.h>
+#endif
 
-    float* ippsMalloc_32fHF(int len) ;
-    void ippsFreeHF(float* ptr);
-    void ippsZero_64fHF(float* ptr, int len);
-    void ippsSum_32fHF(const float* src, int len, float* sum, ippAlgHintFast);
+#ifndef HELPERFUNCS_H
+#define HELPERFUNCS_H
+
+ float* ippsMalloc_32fHF(int len) ;
+ void ippsFreeHF(float* ptr);
+ void ippsZero_64fHF(float* ptr, int len);
+ void ippsSum_32fHF(const float* src, int len, float* sum, ippAlgHintFast);
+
 #endif
