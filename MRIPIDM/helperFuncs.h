cf25f7d72edce4869672f81c0e508a7d09905bb0
ClayBlade <claytonz@nycstudents.net>
2025-07-13 23:09:54 -0400

    Changed the turtle water

--- MRIPIDM/helperFuncs.h
+++ MRIPIDM/helperFuncs.h
@@ -3,7 +3,7 @@
 
  float* ippsMalloc_32fHF(int len) ;
  void ippsFreeHF(float* ptr);
- void ippsZero_64fHF(float* ptr, int len);
+ void ippsZero_64fHF(double* ptr, int len);
  void ippsSum_32fHF(const float* src, int len, float* sum);
 
 #endif
