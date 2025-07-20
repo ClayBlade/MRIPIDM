c5de3debeddfbef1a28eb849bad0b52b8c7bba52
ClayBlade <claytonz@nycstudents.net>
2025-07-20 12:45:59 -0400

    pa

--- MRIPIDM/DoScanAtGPU.cu
+++ MRIPIDM/DoScanAtGPU.cu
@@ -1060,9 +1060,9 @@ for (int i = 0; i < MaxStep; i++){
 
                                                 /*Print doesn't work because i++ in while loop */
                     std::cout << "------" << std::endl;                    
-                    std::cout << i << "Mx: " << Mx[263809] << std::endl;
-                    std::cout << i << "My: " << My[263809] << std::endl;
-                    std::cout << i << "Mz: " << Mz[263809] << std::endl;
+                    std::cout << i << "Mx: " << Mx[(SpinMxNum*SpinMxSliceNum)/2] << std::endl;
+                    std::cout << i << "My: " << My[(SpinMxNum*SpinMxSliceNum)/2] << std::endl;
+                    std::cout << i << "Mz: " << Mz[(SpinMxNum*SpinMxSliceNum)/2] << std::endl;
 				//}
 
 
