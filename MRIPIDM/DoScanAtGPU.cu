e8b287565bc7168917a6d948b05ae8fb4e094e94
ClayBlade <claytonz@nycstudents.net>
2025-07-21 18:25:01 -0400

    Dark Determination

--- MRIPIDM/DoScanAtGPU.cu
+++ MRIPIDM/DoScanAtGPU.cu
@@ -473,9 +473,9 @@ int main(){
     dB0 = new float[SpinMxNum * SpinMxSliceNum]; /*Initialized as 0*/
     dWRnd = new float[SpinMxNum * SpinMxSliceNum * (*SpinNum) * (*TypeNum)]; 
     Gzgrid = new float[SpinMxNum * SpinMxSliceNum];
-    Gygrid = new float[SpixSliceNum];
+    Gygrid = new float[SpinMxNum * SpinMSliceNum];
     Gxgrid = new float[SpinMxNum * SpinMxSliceNum];
-nMxNum * SpinM
+
     std::cout << SpinMxNum * SpinMxSliceNum << " spins in total." << std::endl;
     
     /*VCoi*/
