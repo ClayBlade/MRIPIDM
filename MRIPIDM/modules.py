62d4c7e2f6820a007e3c54770d3a4d900de09e89
ClayBlade <claytonz@nycstudents.net>
2025-08-07 13:13:27 -0400

    WATTPAD

--- MRIPIDM/modules.py
+++ MRIPIDM/modules.py
@@ -130,13 +130,13 @@ class UNet(nn.Module):
         super().__init__()
         self.device = device
         self.time_dim = time_dim
-        self.inc = DoubleConv(c_in, 64)
-        self.down1 = Down(64, 128)
-        self.sa1 = SelfAttention(128, 32)
-        self.down2 = Down(128, 256)
-        self.sa2 = SelfAttention(256, 16)
-        self.down3 = Down(256, 256)
-        self.sa3 = SelfAttention(256, 8)
+        self.inc = DoubleConv(c_in, 32)
+        self.down1 = Down(32, 64)
+        self.sa1 = SelfAttention(128, 16)
+        self.down2 = Down(64, 128)
+        self.sa2 = SelfAttention(256, 8)
+        self.down3 = Down(128, 256)
+        self.sa3 = SelfAttention(256, 4)
 
         self.bot1 = DoubleConv(256, 512)
         self.bot2 = DoubleConv(512, 512)
