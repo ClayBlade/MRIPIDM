f4d969c3e2bbcb9a11145203b4ab7bfdc877f6ef
ClayBlade <claytonz@nycstudents.net>
2025-08-19 17:36:42 -0400

    colab ran out of frees

--- MRIPIDM/smallDiffusionTest.py
+++ MRIPIDM/smallDiffusionTest.py
@@ -1,32 +1,35 @@
-#run main
+#loading model
+
 from modules import *
+import torch
 
-def launch():
-    import argparse
-    parser = argparse.ArgumentParser()
-    args, unknown = parser.parse_known_args()
-    args.run_name = "DDPM_Uncondtional"
-    #for colab# args.path = r"/content/MRIPIDM/MRIPIDM/ParametricMaps/slice_0.npy"
-    args.path = "/root/MRIPIDM/MRIPIDM/ParametricMaps/slice_0.npy"
-    args.epochs = 30
-    args.batch_size = 1
-    data = torch.tensor(np.load(args.path)) # data.shape: torch.Size([171, 171, 141, 3]), store on CPU and then access each slice index on the GPU
-    data = data.reshape(data.shape[0], data.shape[3], data.shape[1], data.shape[2])
-    data = pad_to_even(data)
-    print(f"data.shape after padding: {data.shape}") #data.shape after padding: torch.Size([171, 3, 172, 144])
-    height = data.shape[2]
-    width = data.shape[3]
-    args.image_size = (height, width)
-    args.device = "cuda"
-    args.lr = 3e-4
-    args.dtype = torch.float16
-    train(args, data)
+path = r"/content/MRIPIDM/MRIPIDM/ParametricMaps/slice_0.npy"
 
+data = torch.tensor(np.load(path)) # data.shape: torch.Size([171, 171, 141, 3]), store on CPU and then access each slice index on the GPU
+data = data.reshape(data.shape[0], data.shape[3], data.shape[1], data.shape[2])
+data = pad_to_even(data)
 
-if __name__ == '__main__':
-    launch()
+model = UNet(data.shape[1], data.shape[2]).to(device)
 
+model.load_state_dict(torch.load("root\MRIPIDM\MRIPIDM\models\models\DDPM_Uncondtional\ckpt.pt", map_location=torch.device("cpu")))
 
-#The Things They Still have to do:
-#       Implement more diagnostics: FID, IS scores; loss-time plots; quality vs diversity plots
-#       In 16x16 matrix, converge for simple bloch vectors
+model.eval()
+
+height = data.shape[2]
+width = data.shape[3]
+image_size = (height, width)
+
+diffusion = Diffusion(img_size=image_size, device=device)
+
+sampled_images = diffusion.sample(model, n=5)
+
+mse = nn.MSELoss()
+
+for i, Mhat in enumerate(sampled_images):
+  smallest_difference = 1
+  for j, M in enumerate(data):
+    difference = mse(M, Mhat)
+    if (difference < smallest_difference):
+        smallest_difference = difference
+  print(f"image_{i} smallest_difference: {smallest_difference}")
+
