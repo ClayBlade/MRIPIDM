2f9ef612e57933faf432cc88432a3d45bfce031f
ClayBlade <claytonz@nycstudents.net>
2025-08-28 17:27:15 -0400

    GPU testing

--- MRIPIDM/main.py
+++ MRIPIDM/main.py
@@ -20,10 +20,10 @@ def launch(data, model_run_type):
 if __name__ == '__main__':
     model_run_type = "main"
     model_run_location = "VM"
-    if model_run_type != "VM":
-      from modules import *
+    if model_run_location == "colab":
       path = r"/content/MRIPIDM/MRIPIDM/ParametricMaps/slice_24.npy"
     else:
+      from modules import *
       path = r"/root/MRIPIDM/MRIPIDM/ParametricMaps/slice_24.npy"
     data = torch.tensor(np.load(path)) # data.shape: torch.Size([171, 171, 141, 3]), store on CPU and then access each slice index on the GPU
     data = data.reshape(data.shape[0], data.shape[3], data.shape[1], data.shape[2])
